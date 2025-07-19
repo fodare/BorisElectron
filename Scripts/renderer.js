39fb1c3831a1a75b6eaabd569a7e056c850c722b
fodare <folorunsodamilare007@hotmail.com>
2025-07-19 02:41:56 +0200

    Initial commit.

---
+++ Scripts/renderer.js
@@ -0,0 +1,48 @@
+function getMasterPassWordInput() {
+   return document.getElementById("masterPasswordInput")?.value.trim();
+}
+
+function setStatusMessage(message) {
+   const messageElement = document.getElementById("statusMessage");
+   if (messageElement) {
+      messageElement.textContent = message;
+      setTimeout(() => {
+         messageElement.textContent = "";
+      }, 3500);
+   }
+}
+
+async function handleLogin() {
+   const passwordInput = getMasterPassWordInput();
+   if (!passwordInput) {
+      setStatusMessage("Please enter a vlaid master-password!");
+      return;
+   }
+   const loginStatus = await window.electronAPI.login(passwordInput);
+   console.log(loginStatus);
+}
+
+async function handleRegister() {
+   const passwordInput = getMasterPassWordInput();
+   if (!passwordInput) {
+      setStatusMessage("Please enter a vlaid master-password!");
+      return;
+   }
+   const registrationStatus = await window.electronAPI.createMasterPassword(
+      passwordInput
+   );
+   console.log(registrationStatus);
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+   const path = window.location.pathname;
+
+   if (path.endsWith("login.html")) {
+      document
+         .getElementById("loginBtn")
+         ?.addEventListener("click", handleLogin);
+      document
+         .getElementById("registerBtn")
+         ?.addEventListener("click", handleRegister);
+   }
+});
