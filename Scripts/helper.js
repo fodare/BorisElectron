20fbefdb90273033ebf9988982e734399b772e9d
fodare <folorunsodamilare007@hotmail.com>
2025-07-28 17:15:17 +0200

    Add function for table interactions, hot keys.

--- Scripts/helper.js
+++ Scripts/helper.js
@@ -53,9 +53,78 @@ async function injectNavbar() {
    }
 }
 
+function addTableInteractions(tableBodyId) {
+   const tableBody = document.getElementById(tableBodyId);
+   if (!tableBody) {
+      return;
+   }
+
+   let selectedRow = null;
+   let selectedCell = null;
+   let clipboardClearTimeout = null;
+
+   // Todo: Finish this
+   //    function clearSelection() {
+   //       tableBody
+   //          .querySelectorAll("td")
+   //          .forEach((td) => (td.style.backgroundColor = ""));
+   //       tableBody
+   //          .querySelectorAll("tr")
+   //          .forEach((tr) => (tr.style.backgroundColor = ""));
+   //    }
+
+   tableBody.addEventListener("click", (event) => {
+      const target = event.target;
+      if (target.tagName === "TD") {
+         selectedCell = target;
+         selectedRow = target.parentElement;
+
+         //  clearSelection();
+         //  selectedCell.style.backgroundColor = "#d0f00";
+         //  selectedCell.style.backgroundColor = "#f0d00";
+      }
+   });
+
+   tableBody.addEventListener("dblclick", (event) => {
+      const target = event.target;
+      if (target.tagName === "TD") {
+         // Todo: Handle update logic here
+         //  alert(`Double click event ${target.textContent}`);
+         return;
+      }
+   });
+
+   document.addEventListener("keydown", (event) => {
+      if (!selectedCell) {
+         return;
+      }
+
+      if ((event.ctrlKey || event.metaKey) && event.key.toLowerCase() === "c") {
+         event.preventDefault();
+         const copiedText = selectedCell.textContent;
+         navigator.clipboard
+            .writeText(copiedText)
+            .then(() => {
+               setTimeout(() => {
+                  if (clipboardClearTimeout) {
+                     clearTimeout(clipboardClearTimeout);
+                  }
+                  clipboardClearTimeout = setTimeout(() => {
+                     navigator.clipboard
+                        .writeText("")
+                        .catch((error) => setStatusMessage(error));
+                  }, 3000);
+               });
+            })
+            .catch((error) => setStatusMessage(error));
+      }
+   });
+}
+
 export {
    isMasterPasswordExist,
    getMasterPassWordInput,
    setStatusMessage,
    injectNavbar,
+   addTableInteractions,
 };
