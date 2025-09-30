cce7dd6f4707ae63da5b7818cf80a9217114cd52
fodare <folorunsodamilare007@hotmail.com>
2025-09-30 07:10:00 +0200

    Add some test for some workflow.

--- tests/e2e-test.spec.js
+++ tests/e2e-test.spec.js
@@ -1,4 +1,5 @@
 import { test, expect, _electron as electron } from "@playwright/test";
+import { exec } from "child_process";
 import fs from "fs";
 import os from "os";
 import path from "path";
@@ -7,6 +8,8 @@ test.describe("End-to-end Test", () => {
    let electronApp;
    let window;
    const dummyPassword = "test";
+   const wrongPassword = "test1";
+   const testAccountName = "Test Account 1";
 
    //#region Helpers
 
@@ -95,6 +98,102 @@ test.describe("End-to-end Test", () => {
       await register(window);
       await login(window);
       await expect(window).toHaveTitle("Boris - Credentials");
+      await expect(window.locator("#nav-div")).toBeVisible();
+      await expect(window.locator("#addAccountBtn")).toBeVisible();
+      await expect(window.locator("#credentialsTree")).toBeVisible;
+   });
+
+   test("should display error when logging in with wrong password", async () => {
+      await register(window);
+      await login(window, wrongPassword);
+
+      const toastHeader = window.locator(".toast-header");
+      const toastBody = window.locator(".toast-body");
+      await expect(toastHeader).toBeVisible();
+      await expect(toastBody).toHaveText("Incorrect master password.");
+   });
+
+   test("should navigate to the finances page after login", async () => {
+      await register(window);
+      await login(window);
+
+      const financeLink = window.locator("#financeLink");
+      const addTransactionButton = window.locator("#addTransctionBtn");
+      const transactionsTree = window.locator("#transactionsTree");
+
+      await expect(financeLink).toBeVisible();
+      await financeLink.click();
+
+      await expect(window).toHaveTitle("Boris - Finances");
+      await expect(addTransactionButton).toBeVisible();
+      await expect(transactionsTree).toBeVisible();
+   });
+
+   test("should show confirmation modal when resetting master password", async () => {
+      await register(window);
+
+      const forgotPasswordButton = window.locator("#forgotPasswordBtn");
+      const masterPasswordInput = window.locator("#masterPasswordInput");
+      const registerButton = window.locator("#registerBtn");
+
+      const confirmModal = window.locator("#confirmMessage");
+      const confirmNoButton = window.locator("#confirmNo");
+
+      const toastHeader = window.locator(".toast-header");
+      const toastBody = window.locator(".toast-body");
+
+      await forgotPasswordButton.click();
+      await masterPasswordInput.fill(dummyPassword);
+      await registerButton.click();
+
+      // Assert
+      await expect(confirmModal).toBeVisible();
+
+      await confirmNoButton.click();
+
+      await expect(toastHeader).toBeVisible();
+      await expect(toastBody).toContainText("Registration cancelled");
+   });
+
+   test("should allow adding new account to saved credentials", async () => {
+      await register(window);
+      await login(window);
+
+      await window.locator("#addAccountBtn").click();
+      const newAccountWindow = await electronApp.waitForEvent("window");
+
+      await newAccountWindow.locator("#generateAccountBtn").click();
+      const userNameInputCount = await newAccountWindow
+         .locator("#usernameInput")
+         .count();
+
+      await newAccountWindow.locator("#accountNameInput").fill(testAccountName);
+
+      expect(userNameInputCount).toBeGreaterThan(0);
+      await newAccountWindow.locator("#addAccountBtn").click();
+      await expect(newAccountWindow.locator(".toast-body")).toHaveText(
+         "Wrote account to file!"
+      );
+      await newAccountWindow.close();
+      const accountNameCell = window.locator(`text=${testAccountName}`);
+      await expect(accountNameCell).toBeVisible();
+      await expect(accountNameCell).toHaveText(testAccountName);
+   });
+
+   test("should prevent adding a new account with an empty name", async () => {
+      await register(window);
+      await login(window);
+
+      const addAccountButton = window.locator("#addAccountBtn");
+      await addAccountButton.click();
+
+      const newAccountWindow = await electronApp.waitForEvent("window");
+
+      const submitAccountButton = newAccountWindow.locator("#addAccountBtn");
+      await submitAccountButton.click();
+
+      const errorToast = newAccountWindow.locator(".toast-body");
+      await expect(errorToast).toHaveText("Account name is required!");
    });
 
    //#endregion
